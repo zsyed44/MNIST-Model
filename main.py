aaff858d0870bde2303baf6ffece700ba03e3706
zain <zsyed44@uwo.ca>
2025-01-16 16:42:57 -0500

    Implemented the Training CNN code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy
 import matplotlib.pyplot as plot
 import seaborn # NOT a mandatory library, just allows us to visualize our datasets
 
+#print(tensorflow.__version__) # Checking the version of tensorflow we are using
+#print(numpy.__version__) # Checking the version of numpy we are using
 
 # Downloading and Loading the MNIST dataset from tensorflow
 MNIST = tensorflow.keras.datasets.mnist
@@ -35,3 +37,58 @@ y_test = tensorflow.one_hot(y_test.astype(numpy.int32), depth=10)
 # We can visualize one od the images in the dataset to see what it looks like if we would like. replace "14" with the number of the image you wish to visualize
 plot.imshow(x_train[14][:,:,0])
 print(y_train[14])
+
+# Defining necessary variables for our model
+batchSize = 64 # Play around with this value to see how it effects the training of the model
+epochs = 5 # Play around with this value to see how it effects the training of the model
+numClasses = 10 # Probably best to not play with this one, it effects our categorized outputs
+
+#Defining our model
+layer = tensorflow.keras.layers # Optinally adding a variable to access tensorflow.keras.layers in a much easier manner
+model = tensorflow.keras.models.Sequential([ # Creating a sequential model, where we add layers to the model in a sequential manner(one after the other)
+    
+    # ReLU is an activation function which helps the network learn complex patterns in the data, it is the most widely used activation function 
+    layer.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape), # Our first convolutional layer, we are using 32 5x5 pixel filters to scan over the image and detect patterns. At this layer we define our input shape as well
+    layer.Conv2D(32, (5,5), padding='same', activation='relu'), # Our second convolutional layer, which enforces what we learned in the first layer, with the same filters, for more complex pattern recognition
+    
+    layer.MaxPool2D(), # The Max Pooling layer reduces the image size by taking the maximum value of each 2x2 block, reducing our image from 28x28 to 14x14
+    layer.Dropout(0.25), # This drops 25% of neurons in the layer, to prevent overfitting
+    
+    layer.Conv2D(64, (3,3), padding='same', activation='relu'), # Since our image is now 14x14, we can use smaller filters to detect finer details and patterns
+    layer.Conv2D(64, (3,3), padding='same', activation='relu'), # padding='same' keeps the output the same size as the input
+    
+    layer.MaxPool2D(strides=(2,2)), # Further reduce image size, this time the 2x2 grid is explicitly defined, reducing our image to 7x7 pixels
+    layer.Dropout(0.25), # Drops 25% of neurons
+    layer.Flatten(), # Flattens our 2D feature maps (7x7x64) to a 1D vector
+    layer.Dense(128, activation='relu'), # A fully connected layer with 128 neurons, each in charge of learning a complex combination of patterns and previous features
+    layer.Dropout(0.5), # Final dropout, drops 50% of neurons at random
+
+    # softmax is another activation function, but it is used in the final layer of a classification network, it outputs probabilities of each class
+    # An example could be [0.01, 0.05, 0.02, 0.88, 0.01, 0.01, 0.00, 0.00, 0.00, 0.01], where the highest value is the predicted class (in this case the number 3 at 88%)
+    layer.Dense(numClasses, activation='softmax') # Our final layer, with 10 neurons, one for each number 0-9
+
+])
+
+# Here is where we compile our model
+model.compile(
+    optimizer=tensorflow.keras.optimizers.RMSprop(epsilon=1e-08), # RMSprop is an optimizer that helps us train our model, epsilon is a small value to prevent division by zero
+    loss='categorical_crossentropy', # This is the loss function, it measures how well our model is doing, and adjusts the weights of the model to minimize this value
+    metrics=['acc'] # This is the metric we are using to measure the performance of our model, in this case we are using accuracy
+)
+
+# This simple function fits our training data, if our accuracy reaches 99.5% or higher, it stops training (to provent overfitting and reduce use of time and resources)
+class myCallback(tensorflow.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.995):
+      print("\nReached 99.5% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+# This is where we train our model, we input our training data, batch size, epochs, and validation split
+history = model.fit(x_train, y_train,
+                    batch_size=batchSize,
+                    epochs=epochs,
+                    validation_split=0.1, # Represents the 10% of images in the dataset reserved for testing accuracy
+                    callbacks=[callbacks]
+                )
